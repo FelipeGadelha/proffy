b4b9f014b43447f0836d4126b0dec758221c4ee9
Felipe Gadelha Diniz da Silva <felipe.gadelha@cybersolutions.com.br>
2021-05-31 09:06:12 -0300

    created projetc

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import Landing from "./pages/Landing"
+import "./styles/global.css"
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+}
+ 
+export default App;
